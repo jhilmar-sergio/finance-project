c21505a10fe07780327717dde9722f0a432a661f
jhilmar1 <jhilmar.contacto@gmail.com>
2016-01-19 13:10:31 -0400

    actualizacion footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,12 +1,16 @@
             </div>
 
             <div id="footer-principal" class="col-xs-12">
-                 Desarrolado por: <a href="http://codepen.io/jhilmar1/full/mVJzJb" target="_blank">Jhilmar</a> junto a <a href="http://cs50.tv" target="_blank">cs50.tv</a> <br>
-                 Email: <a href="mailto:jhilmar.contacto@gmail.com" target="_blank">jhilmar.contacto@gmail.com</a>
+            	<div class="row clearfix">
+            		<div class="col-xs-12 col-sm-8">
+            		<p>Desarrolado por: <a href="http://codepen.io/jhilmar1/full/mVJzJb" target="_blank">Jhilmar</a> junto a <a href="http://cs50.tv" target="_blank">cs50.tv</a></p>
+                 <p>Email: <a href="mailto:jhilmar.contacto@gmail.com" target="_blank">jhilmar.contacto@gmail.com</a></p>
+            	</div>
+            	<div class="col-xs-12 col-sm-4">
+            		<span><a href="https://github.com/jhilmar1/finance-project">Proyecto en github</a></span>
+            	</div>
+            	</div>            	                
             </div>
-
         </div>
-
     </body>
-
 </html>
